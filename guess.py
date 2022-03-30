08251cfe2ba37c08bcb05ee5a3161505a5cbd589
Matheus <matheusc200@gmail.com>
2022-03-29 21:58:32 -0300

    now its ready

---
+++ guess.py
@@ -0,0 +1,47 @@
+# Importing the libraries
+import random
+
+
+# The introduction for the user
+print("************************")
+print("      Guess Game         ")
+print("************************")
+nick = input(f"Hey, whats your name?\n")
+
+# The endgame image
+def win():
+    print("************************")
+    print("      You won!         ")
+    print("************************")
+def lose():
+    print("************************")
+    print("      You lost!         ")
+    print("************************")
+
+# Creating the difficulty sistem
+dif = int(input(f"Now, {nick}, choose one of these difficulties: \n 1: Easy; \n 2: Moderate \n 3: Hard\n"))
+value = dif * 10
+tries = int(10 / dif)
+# Creating the loop and the game system
+def guess(value, tries):
+    random_number = random.randint(1, value)
+    guess = 0
+    while guess != random_number and tries != 0:
+        guess = int(input("What number you choose?"))
+        if guess > random_number:
+            print("This is a really high value, try again.")
+            tries = tries -1
+            print(f"You have {tries} left")
+        elif guess <random_number:
+            print("Try an big one.")
+            tries = tries -1
+            print(f"You have {tries} left")
+    if tries == 0:
+        lose()
+        print(f"The number was {random_number}!")
+    elif tries > 0:
+        win()
+        print(f"The number was {random_number}!")
+
+# Getting the game working:
+guess(value, tries)
---
+++ mad_game.py
@@ -0,0 +1,19 @@
+# A ideia desse mini-game é criar uma frase aleatória com os inputs dos usuários.
+
+#Iniciando uma pequena apresentação
+print("Bem vindo ao minigame! A seguir você receberá instruções de tipos de itens e deverá responder com um adequado. \n Por exemplo: verbo -> Criar")
+nick = input("Digite o seu nome para continuar:")
+
+#Abaixo as variáveis para montar a frase
+adj1 = input("Adjetivo 1:")
+famoso = input("Famoso:")
+verbo = input("Verbo 1:")
+adj2 = input("Segundo adjetivo:")
+coisa = input("Qual coisa?")
+cheiro = input("Escolha um cheiro:")
+
+#Por fim, a frase sendo printada:
+print(f"O mundo seria muito melhor se você fosse mais {adj1}. \
+Aliás, você sabia que o {famoso} consegue {verbo}?\
+Eu também não sabia, mas é verdade. Outro fato deveras {adj2} é que {famoso} vem do latin \
+Significa {coisa}. Recebeu esse nome por ter cheiro de {cheiro}")
