f35302560b4d5430ecb1abe9173d34b9617ee3c5
Matheus <matheusc200@gmail.com>
2022-04-04 18:27:35 -0300

    New learning folder for JS

--- Learning/Python/project022.py
+++ Learning/Python/project022.py
@@ -6,4 +6,3 @@ print(f"Your name in upper is: {name.upper()}")
 print(f"Your name in lower is: {name.lower()}")
 print(f"Your name has {len(name) - name.count(' ')} letters.")
 print(f"Your first name has {name.find(' ')} letters.")
-
